82cd6b133aec38895d48f511c7c5c1d77f0e92f5
Bill Jones <williamjones120862@gmail.com>
2025-02-10 19:37:16 -0500

    Allowed for multiple concurrent users

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -3,28 +3,17 @@
 # This controller takes the User input data and formats an address that is used to get the
 # the forecast data that is then displayed in the view.
 class WeatherController < ApplicationController
-  @address = ''
-
-  def self.return_address
-    @address
-  end
-
-  def self.put_address=(value)
-    @address = value
-  end
-
   def dashboard
-    return if WeatherController.return_address.blank?
+    return if session[:address].blank?
 
-    address_string = WeatherController.return_address
-    latlong_api = GeocoderApi.new(address_string)
+    latlong_api = GeocoderApi.new(session[:address])
     latlong = latlong_api.lat_long_from_address
     @display_name = latlong_api.display_name_from_address
     @forecast_data = get_forecast_data(latlong)
   end
 
   def address
-    WeatherController.put_address = ('')
+    session[:address] = ''
     param_array = load_param_array(params)
     format_address(param_array) if param_array.length.positive?
     redirect_back_or_to weather_dashboard_path
@@ -34,7 +23,7 @@ class WeatherController < ApplicationController
 
   def get_forecast_data(latlong)
     @from_cache = true
-    Rails.cache.fetch("#{latlong[:lat]}#{latlong[:long]}", expires_in: 30.minutes) do
+    Rails.cache.fetch("#{latlong[:lat]}#{latlong[:long]}", expires_in: 30.minutes, race_condition_ttl: 10.seconds) do
       @from_cache = false
       weather_api = NationalWeatherApi.new(latlong)
       weather_api.weather_by_lat_long
@@ -43,12 +32,11 @@ class WeatherController < ApplicationController
 
   def format_address(param_array)
     param_array.each do |param|
-      if WeatherController.return_address.blank?
-        WeatherController.put_address = (param)
-      else
-        address_value = WeatherController.return_address
-        WeatherController.put_address = ("#{address_value},#{param}")
-      end
+      session[:address] = if session[:address].blank?
+                            param
+                          else
+                            "#{session[:address]},#{param}"
+                          end
     end
   end
 
